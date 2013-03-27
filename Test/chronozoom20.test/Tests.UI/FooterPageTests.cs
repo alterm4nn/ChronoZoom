5a527d28b5d84ff1d8398e8a9b945dc4d688ed90
alexey chumagin <v-alchum@microsoft.com>
2013-03-27 15:27:07 +0400

    added class for Footer Tests

---
+++ Test/chronozoom20.test/Tests.UI/FooterPageTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    public class FooterPageTests : TestBase
+    {
+
+        #region Initialize and Cleanup
+        public TestContext TestContext { get; set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            BrowserStateManager.RefreshState();
+            NavigationHelper.OpenHomePage();
+            WelcomeScreenHelper.CloseWelcomePopup();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            CreateScreenshotsIfTestFail(TestContext);
+        }
+
+        #endregion
+    }
+}
