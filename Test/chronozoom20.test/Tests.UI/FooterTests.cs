55eeec52dfe4333156c58c07a6298981302e1b9f
alexey chumagin <v-alchum@microsoft.com>
2013-03-29 16:34:13 +0400

    added architect tests for footer

---
+++ Test/chronozoom20.test/Tests.UI/FooterTests.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    public class FooterTests : TestBase
+    {
+        #region Initialize and Cleanup
+        public TestContext TestContext { get; set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            BrowserStateManager.RefreshState();
+            NavigationHelper.OpenHomePage();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            CreateScreenshotsIfTestFail(TestContext);
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void MrcLink()
+        {
+
+        }
+        
+        [TestMethod]
+        public void UcBerkelyLink()
+        {
+
+        }
+        
+        [TestMethod]
+        public void MsuLink()
+        {
+
+        }
+        
+        [TestMethod]
+        public void TakeOurSurveyLink()
+        {
+
+        }
+        
+        [TestMethod]
+        public void ReportAProblemLink()
+        {
+
+        } 
+        
+        [TestMethod]
+        public void BehindTheScenesLink()
+        {
+
+        }
+        
+        [TestMethod]
+        public void TermsOfUseLink()
+        {
+
+        }
+        
+        [TestMethod]
+        public void PrivacyLink()
+        {
+
+        }
+        
+        [TestMethod]
+        public void TrademarkLink()
+        {
+
+        }
+
+
+
+
+        
+    }
+}
