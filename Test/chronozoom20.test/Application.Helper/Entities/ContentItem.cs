6a41d54f91bd5d2873894e47784a157585c8e0f1
alexey chumagin <v-alchum@microsoft.com>
2013-04-01 18:23:02 +0400

    Classes ContentItem and Exhibit have been implemented

--- Test/chronozoom20.test/Application.Helper/Entities/ContentItem.cs
+++ Test/chronozoom20.test/Application.Helper/Entities/ContentItem.cs
@@ -2,6 +2,9 @@
 {
     public class ContentItem
     {
-         
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Uri { get; set; }
+        public string MediaType { get; set; }
     }
 }
--- Test/chronozoom20.test/Application.Helper/Entities/Exhibit.cs
+++ Test/chronozoom20.test/Application.Helper/Entities/Exhibit.cs
@@ -1,7 +1,12 @@
-﻿namespace Application.Helper.Entities
+﻿using System.Collections.Generic;
+
+namespace Application.Helper.Entities
 {
     public class Exhibit
     {
-         
+        public double Time { get; set; }
+        public string Title  { get; set; }
+        public string Description  { get; set; }
+        public List<ContentItem> ContentItems = new List<ContentItem>();
     }
 }
