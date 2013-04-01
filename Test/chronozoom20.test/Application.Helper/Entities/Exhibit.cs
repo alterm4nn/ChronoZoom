d865a030a40f6630b9297848f086dfc186ddc181
alexey chumagin <v-alchum@microsoft.com>
2013-04-01 18:32:54 +0400

    Added Timline entity

--- Test/chronozoom20.test/Application.Helper/Entities/Exhibit.cs
+++ Test/chronozoom20.test/Application.Helper/Entities/Exhibit.cs
@@ -7,11 +7,11 @@ namespace Application.Helper.Entities
         public double Time { get; set; }
         public string Title  { get; set; }
         public string Description  { get; set; }
-        public List<ContentItem> ContentItems = new List<ContentItem>();
+        public List<ContentItem> ContentItems { get; set; }
 
         public override string ToString()
         {
-            return string.Format("[ContentItem: Title = {0}, Description = {1}, Time = {2}, ContentItems = {3}]", Title, Description, Time, ContentItems.ToString());
+            return string.Format("[Exhibit: Title = {0}, Description = {1}, Time = {2}, ContentItems = {3}]", Title, Description, Time, ContentItems.ToString());
         }
     }
 }
