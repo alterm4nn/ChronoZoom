e2f41a64d93854bb28b878850efc745a5a8158bc
Javier Luraschi <javierluraschi@hotmail.com>
2013-04-07 08:21:01 -0700

    Entities Test and Code Analysis

--- Source/Chronozoom.Entities/Migration/Migrator.cs
+++ Source/Chronozoom.Entities/Migration/Migrator.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Globalization;
+using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Text;
@@ -30,6 +31,14 @@ namespace Chronozoom.Entities.Migration
             return ConfigurationManager.AppSettings["BaseCollectionsAdministrator"];
         });
 
+        private static Lazy<string> _baseDirectory = new Lazy<string>(() =>
+        {
+            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["BaseDataMigrationDirectory"]))
+                return ConfigurationManager.AppSettings["BaseDataMigrationDirectory"];
+
+            return AppDomain.CurrentDomain.BaseDirectory;
+        });
+
         public Migrator(Storage storage)
         {
             _storage = storage;
@@ -50,9 +59,9 @@ namespace Chronozoom.Entities.Migration
             {
                 // Load the Beta Content collection
                 Collection collection = LoadCollections(superCollectionName, superCollectionName, contentAdminId);
-                using (Stream getData = File.OpenRead(AppDomain.CurrentDomain.BaseDirectory + @"Dumps\" + getFileName))
-                using (Stream getToursData = getToursFileName == null ? null : File.OpenRead(AppDomain.CurrentDomain.BaseDirectory + @"Dumps\" + getToursFileName))
-                using (Stream getThresholdsData = getThresholdsFileName == null ? null : File.OpenRead(AppDomain.CurrentDomain.BaseDirectory + @"Dumps\" + getThresholdsFileName))
+                using (Stream getData = File.OpenRead(_baseDirectory.Value + @"Dumps\" + getFileName))
+                using (Stream getToursData = getToursFileName == null ? null : File.OpenRead(_baseDirectory.Value + @"Dumps\" + getToursFileName))
+                using (Stream getThresholdsData = getThresholdsFileName == null ? null : File.OpenRead(_baseDirectory.Value + @"Dumps\" + getThresholdsFileName))
                 {
                     LoadData(getData, getToursData, getThresholdsData, collection, replaceGuids);
                 }
@@ -298,16 +307,11 @@ namespace Chronozoom.Entities.Migration
             public T d { get; set; }
         }
 
-        private delegate void TraverseOperation(Timeline timeline);
-        private void TraverseTimelines(IEnumerable<Timeline> timelines, TraverseOperation operation)
+        public static void TraverseTimelines(IEnumerable<Timeline> timelines, TraverseOperation operation)
         {
-            if (timelines == null)
-                return;
-
             foreach (Timeline timeline in timelines)
             {
-                operation(timeline);
-                TraverseTimelines(timeline.ChildTimelines, operation);
+                timeline.Traverse(operation);
             }
         }
 
