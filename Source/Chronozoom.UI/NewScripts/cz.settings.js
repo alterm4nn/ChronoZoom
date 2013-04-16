8a3c1368ef8a69fa23b523112ab09f9eb0496c12
alex-zen <alexz@mstlab.org>
2013-04-16 12:35:18 +0400

    Hot fix for production regime links. Added updated .js file

--- Source/Chronozoom.UI/NewScripts/cz.settings.js
+++ Source/Chronozoom.UI/NewScripts/cz.settings.js
@@ -166,10 +166,10 @@ var CZ;
         Settings.minSmallTickSpace = 8;
         Settings.timescaleThickness = 2;
         Settings.cosmosTimelineID = "00000000-0000-0000-0000-000000000000";
-        Settings.earthTimelineID = "7f9656a7-a3f8-48cc-8b1c-18c9779ef232";
-        Settings.lifeTimelineID = "aa6538d2-9ad0-4d42-b3cc-47bfed233bca";
-        Settings.prehistoryTimelineID = "f502a3aa-dbdc-43c8-8863-a8205db7fef5";
-        Settings.humanityTimelineID = "8c24447f-1072-465d-b03b-1cac269b5ded";
+        Settings.earthTimelineID = "8c8f294e-b09e-4506-94a3-5a51f0fb7a0b";
+        Settings.lifeTimelineID = "06541ae4-1095-4658-a8eb-e2cbc1371d82";
+        Settings.prehistoryTimelineID = "753022cb-1fac-4814-8abe-1cf0918f3f12";
+        Settings.humanityTimelineID = "a14d474d-121f-470d-b101-1c7727f1b69d";
         Settings.toursAudioFormats = [
             {
                 ext: 'mp3'
