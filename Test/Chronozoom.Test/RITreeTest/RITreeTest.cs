56be5219bc80a070d02f3c9f6db6a1ce48764bba
Yitao Li <yl790@uw.edu>
2013-04-16 15:08:53 -0700

    adding assert statements

--- Test/Chronozoom.Test/RITreeTest/RITreeTest.cs
+++ Test/Chronozoom.Test/RITreeTest/RITreeTest.cs
@@ -74,12 +74,12 @@ namespace Chronozoom.Test.RITreeTest
         [TestMethod]
         public void TestRITree()
         {   /* test correctness of ForkNode computations */
-            Int64 count, test_start_time = -7333311, test_end_time = 2013;
+            Int64 count1, count2, test_start_time = -7333311, test_end_time = 2013;
             Int64 start = test_start_time + 13700000001, end = test_end_time + 13700000001;
-            Console.WriteLine(Storage.ForkNode(offset + 3, offset + 7));  //should output 4
-            Console.WriteLine(Storage.ForkNode(offset + 29515, offset + 30500));  //should output 29696
-            Console.WriteLine(Storage.ForkNode(offset + 13700005424, offset + 13700005585));  //should output 13700005504
-            Console.WriteLine(Storage.ForkNode(offset + 3970765138, offset + 6840749504));  //should output 4294967296
+            Debug.Assert(Storage.ForkNode(offset + 3, offset + 7) == 4);
+            Debug.Assert(Storage.ForkNode(offset + 29515, offset + 30500) == 29696);
+            Debug.Assert(Storage.ForkNode(offset + 13700005424, offset + 13700005585) == 13700005504);
+            Debug.Assert(Storage.ForkNode(offset + 3970765138, offset + 6840749504) == 4294967296);
             /* check (via random sampling) that the correct ForkNode computation is actually applied to records in DB */
             var r = _storage.Database.SqlQuery<Timeline>("SELECT TOP 10 * FROM Timelines ORDER BY NEWID()");
             foreach (Timeline t in r)
@@ -94,27 +94,28 @@ namespace Chronozoom.Test.RITreeTest
             long t1 = DateTime.Now.Ticks;
             var r1 = _storage.Database.SqlQuery<Timeline>("SELECT * FROM Timelines WHERE (FromYear >= {0} AND FromYear <= {1}) OR (ToYear >= {0} AND ToYear <= {1}) OR (FromYear >= {0} AND ToYear <= {1}) OR (FromYear <= {0} AND ToYear >= {1})", test_start_time, test_end_time);
             long t2 = DateTime.Now.Ticks;
-            count = 0;
+            count1 = 0;
             Console.WriteLine("time elapsed: " + ((t2 - t1) * 1.0 / TimeSpan.TicksPerMillisecond) + "ms");
             foreach (var r1_t in r1)
             {
                 Console.WriteLine(r1_t.Id + ", " + r1_t.FromYear + "," + r1_t.ToYear + "," + r1_t.ForkNode);
-                ++count;
+                ++count1;
             }
-            Console.WriteLine(count + " timeline(s) returned");
+            Console.WriteLine(count1 + " timeline(s) returned");
             /* test RI tree query */
             Console.WriteLine("testing RI-tree query:");
             t1 = DateTime.Now.Ticks;
             var r2 = _storage.Database.SqlQuery<Timeline>("SELECT DISTINCT [Timelines].* FROM [Timelines] JOIN (SELECT (CAST({2} AS BIGINT) & [B1]) AS [node] FROM [Bitmasks] WHERE (CAST({2} AS BIGINT) & [B2]) <> 0) AS [left_nodes] ON [Timelines].[ForkNode] = [left_nodes].[node] AND [Timelines].[ToYear] >= {0} UNION ALL SELECT DISTINCT [Timelines].* FROM [Timelines] JOIN (SELECT ((CAST({3} AS BIGINT) & [B1]) | [B3]) AS [node] FROM [bitmasks] WHERE (CAST({3} AS BIGINT) & [B3]) = 0) AS [right_nodes] ON [Timelines].[ForkNode] = [right_nodes].[node] AND [Timelines].[FromYear] <= {1} UNION ALL SELECT DISTINCT [Timelines].* FROM [Timelines] WHERE [Timelines].[ForkNode] BETWEEN {2} AND {3}", test_start_time, test_end_time, start, end);
             t2 = DateTime.Now.Ticks;
-            count = 0;
+            count2 = 0;
             Console.WriteLine("time elapsed: " + ((t2 - t1) * 1.0 / TimeSpan.TicksPerMillisecond) + "ms");
             foreach (var r2_t in r2)
             {
                 Console.WriteLine(r2_t.Id + ", " + r2_t.FromYear + "," + r2_t.ToYear + "," + r2_t.ForkNode);
-                ++count;
+                ++count2;
             }
-            Console.WriteLine(count + " timeline(s) returned");
+            Console.WriteLine(count2 + " timeline(s) returned");
+            Debug.Assert(count1 == count2);
         }
     }
 }
