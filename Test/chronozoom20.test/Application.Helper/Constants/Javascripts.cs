a55fd931826d9d7c70e78ed3d3e1c6ed5a2614b3
alexey chumagin <v-alchum@microsoft.com>
2013-04-18 12:49:10 +0400

    Fix: new_timeline_should_have_a_title: script is broken

--- Test/chronozoom20.test/Application.Helper/Constants/Javascripts.cs
+++ Test/chronozoom20.test/Application.Helper/Constants/Javascripts.cs
@@ -3,12 +3,12 @@
     public class Javascripts
     {
         public const string LastCanvasElement =
-            "vc.data('ui-virtualCanvas')._layersContent.children[0].children[vc.data('ui-virtualCanvas')._layersContent.children[0].children.length - 1]";
+            "$('#vc').data('ui-virtualCanvas')._layersContent.children[0].children[$('#vc').data('ui-virtualCanvas')._layersContent.children[0].children.length - 1]";
 
         public const string Cosmos =
-            "vc.data('ui-virtualCanvas')._layersContent.children[0]";
+            "$('#vc').data('ui-virtualCanvas')._layersContent.children[0]";
 
         public const string CosmosChildrenCount =
-            "vc.data('ui-virtualCanvas')._layersContent.children[0].children.length";
+            "$('#vc').data('ui-virtualCanvas')._layersContent.children[0].children.length";
     }
 }
