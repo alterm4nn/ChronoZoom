a55950089698258e1b474bf07c8943652bb3d503
alex-zen <alexz@mstlab.org>
2013-04-24 01:27:46 +0400

    Fixed 'console is undefined' error

--- Source/Chronozoom.UI/NewScripts/cz.ts
+++ Source/Chronozoom.UI/NewScripts/cz.ts
@@ -19,6 +19,13 @@ module CZ {
         };
 
         $(document).ready(function () {
+            //Ensures there will be no 'console is undefined' errors
+            window.console = window.console || <any>(function () {
+                var c = <any>{};
+                c.log = c.warn = c.debug = c.info = c.log =
+                    c.error = c.time = c.dir = c.profile = c.clear = c.exception = c.trace = c.assert = function () { };
+                return c;
+            })();
 
             $('.bubbleInfo').hide();
 
