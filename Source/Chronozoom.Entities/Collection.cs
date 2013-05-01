186d0ca923fef226947155205aa2aa5d11e25779
William French <willum070@yahoo.com>
2013-05-01 09:52:08 -0700

    Added documentation comments; switched on XML generation in the project properties.

--- Source/Chronozoom.Entities/Collection.cs
+++ Source/Chronozoom.Entities/Collection.cs
@@ -11,14 +11,23 @@ using System.Runtime.Serialization;
 
 namespace Chronozoom.Entities
 {
+    /// <summary>
+    /// Represents a collection of timelines.
+    /// </summary>
     [DataContract]
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix")]
     public class Collection
     {
+        /// <summary>
+        /// The ID of the collection.
+        /// </summary>
         [Key]
         [DataMember]
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// The title of the collection.
+        /// </summary>
         [DataMember]
         public string Title { get; set; }
 
@@ -26,6 +35,9 @@ namespace Chronozoom.Entities
         [DataMember]
         public string UserId { get; set; }
 
+        /// <summary>
+        /// The user ID for the collection owner.
+        /// </summary>
         [DataMember]
         public User User { get; set; }
     }
--- Source/Chronozoom.Entities/SearchResult.cs
+++ Source/Chronozoom.Entities/SearchResult.cs
@@ -9,6 +9,9 @@ using System.Runtime.Serialization;
 
 namespace Chronozoom.Entities
 {
+    /// <summary>
+    /// Specifies the type of object contained by the search result.
+    /// </summary>
     [DataContract(Name = "ObjectType")]
     public enum ObjectType
     {
@@ -20,15 +23,27 @@ namespace Chronozoom.Entities
         ContentItem = 2
    }
 
+    /// <summary>
+    /// Contains a search result.
+    /// </summary>
     [DataContract]
     public class SearchResult
     {
+        /// <summary>
+        /// The ID of the search result.
+        /// </summary>
         [DataMember(Name = "id")]
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// The title of the search result.
+        /// </summary>
         [DataMember(Name = "title")]
         public string Title { get; set; }
 
+        /// <summary>
+        /// The type of object contained by the search result.
+        /// </summary>
         [DataMember(Name = "objectType")]
         public ObjectType ObjectType { get; set; }
     }
