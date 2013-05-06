6c794549ae1c701484dcb8d323037d0b9d4aa68b
alexey chumagin <v-alchum@microsoft.com>
2013-05-06 13:17:55 +0400

    BL: Add cases for GA Ma BCE

--- Test/chronozoom20.test/Tests.BL/Scripts/Specs/timelineEditFormSpec.js
+++ Test/chronozoom20.test/Tests.BL/Scripts/Specs/timelineEditFormSpec.js
@@ -33,16 +33,37 @@ describe("Given:  'edit timeline' form is opened: ", function () {
         $('.ui-button-icon-primary.ui-icon.ui-icon-closethick').click();
     });
 
-    //"data = ['timeline title', 'start value', 'end value', 'regime', 'description']"
-    dataTitle = ["", "5", "10", "ce","empty 'title'"];
-    dataStart = ["titlename", "", "5", "ce", "empty 'start'"];
-    dataEnd = ["title", "45", "", "ce", "empty 'end'"];
-    dataStartNotNumber = ["title", "45", "abc", "ce", "'end' is not number"];
-    dataNotNumber = ["title", "!@#", "789", "ce", "'start' is not number"];
-    dataEndLessStart = ["title", "5", "2", "ce", "start is less than end"];
-    dataEndLessStartBce = ["title", "2", "5", "bce", "start is less than end"];
 
-    using("Data set: ", [dataEndLessStart, dataEndLessStartBce, dataTitle, dataStart, dataEnd, dataStartNotNumber, dataNotNumber], function (title, start, end, regime, conditional) {
+
+
+    using("Data set: ", [
+            //"data = ['timeline title', 'start value', 'end value', 'regime', 'description']"
+            ["title", "5", "2", "ce", "ce", "start is less than end"],
+            ["title", "5", "10", "ce", "bce", "start is less than end"],
+            ["title", "5", "10", "ce", "ka", "start is less than end"],
+            ["title", "5", "10", "ce", "ma", "start is less than end"],
+            ["title", "5", "10", "ce", "ga", "start is less than end"],
+            ["title", "2", "-5", "bce", "ce", "start is less than end"],
+            ["title", "2", "5", "bce", "ka", "start is less than end"],
+            ["title", "2", "5", "bce", "ma", "start is less than end"],
+            ["title", "2", "5", "bce", "ga", "start is less than end"],
+            ["title", "2", "5", "bce", "bce", "start is less than end"],
+            ["title", "2", "-5000000", "ma", "ce", "start is less than end"],
+            ["title", "2", "5000000", "ma", "bce", "start is less than end"],
+            ["title", "2", "5000", "ma", "ka", "start is less than end"],
+            ["title", "2", "5", "ma", "ga", "start is less than end"],
+            ["title", "2", "5", "ma", "ma", "start is less than end"],
+            ["title", "2", "0.005", "ka", "ma", "start is less than end"],
+            ["title", "20", "1", "ka", "ga", "start is less than end"],
+            ["title", "2", "5000", "ka", "bce", "start is less than end"],
+            ["title", "2", "-5000", "ka", "ce", "start is less than end"],
+            ["title", "2", "5", "ka", "ka", "start is less than end"],
+            ["title", "2", "5", "ga", "ga", "start is less than end"],
+            ["title", "2", "5e+3", "ga", "ma", "start is less than end"],
+            ["title", "2", "5e+6", "ga", "ka", "start is less than end"],
+            ["title", "2", "5e+9", "ga", "bce", "start is less than end"],
+            ["title", "2", "-5e+9", "ga", "ce", "start is less than end"],
+    ], function (title, start, end, regimeStart, regimeEnd, conditional) {
 
         describe("When: User set: " + conditional + " and save changes", function () {
             beforeEach(function () {
@@ -50,13 +71,67 @@ describe("Given:  'edit timeline' form is opened: ", function () {
                 $('#timelineTitleInput').val(title);
                 $('#timelineStartInput .cz-datepicker-year').val(start);
                 $('#timelineEndInput .cz-datepicker-year').val(end);
-                ($)("#timelineStartInput .cz-datepicker-regime").val(regime).attr("selected", "selected");
-                ($)("#timelineEndInput .cz-datepicker-regime").val(regime).attr("selected", "selected");
+                ($)("#timelineStartInput .cz-datepicker-regime").val(regimeStart).attr("selected", "selected");
+                ($)("#timelineEndInput .cz-datepicker-regime").val(regimeEnd).attr("selected", "selected");
                 $("span:contains('save and close')").click();
             });
 
             it("Then: Error message should be thrown.", function () {
                 expect('block').toEqual($('#TimelineErrorSpan').css('display'));
+            });
+        });
+    });
+
+    var regimes = ["ce", "bce", "ma", "ga", "ka"];
+
+    describe("When: User set start as empty", function () {
+        using1("Data set: ", regimes, function (regime) {
+            describe("And: regime: " + regime + " and save changes", function () {
+                beforeEach(function () {
+                    setValues("text", "", Math.floor((Math.random() * 100) + 1), regime, regime);
+                });
+                it("Then: Error message should be thrown.", function () {
+                    expect('block').toEqual($('#TimelineErrorSpan').css('display'));
+                });
+            });
+        });
+    });
+
+    describe("When: User set start as not number", function () {
+        using1("Data set: ", regimes, function (regime) {
+            describe("And: regime: " + regime + " and save changes", function () {
+                beforeEach(function () {
+                    setValues("text", "!@34*&#", Math.floor((Math.random() * 100) + 1), regime, regime);
+                });
+                it("Then: Error message should be thrown.", function () {
+                    expect('block').toEqual($('#TimelineErrorSpan').css('display'));
+                });
+            });
+        });
+    });
+
+    describe("When: User set end as not number", function () {
+        using1("Data set: ", regimes, function (regime) {
+            describe("And: regime: " + regime + " and save changes", function () {
+                beforeEach(function () {
+                    setValues("text", Math.floor((Math.random() * 100) + 1), "ddd@#$", regime, regime);
+                });
+                it("Then: Error message should be thrown.", function () {
+                    expect('block').toEqual($('#TimelineErrorSpan').css('display'));
+                });
+            });
+        });
+    });
+
+    describe("When: User set end as empty", function () {
+        using1("Data set: ", regimes, function (regime) {
+            describe("And: regime: " + regime + " and save changes", function () {
+                beforeEach(function () {
+                    setValues("text", Math.floor((Math.random() * 100) + 1), "", regime, regime);
+                });
+                it("Then: Error message should be thrown.", function () {
+                    expect('block').toEqual($('#TimelineErrorSpan').css('display'));
+                });
             });
         });
     });
@@ -72,15 +147,45 @@ describe("Given:  'edit timeline' form is opened: ", function () {
             expect($('.cz-datepicker-errormsg:last').text()).toEqual("Year should be a number.");
         });
     });
+
+    describe("When user set empty title", function () {
+        describe("And: save changes", function () {
+            beforeEach(function () {
+                setValues("", 5, 10, "ga", "ga");
+            });
+            it("Then: Error message should be thrown.", function () {
+                expect('block').toEqual($('#TimelineErrorSpan').css('display'));
+            });
+        });
+    });
 });
+
+function using1(name, values, func) {
+    for (var i = 0, count = values.length; i < count; i++) {
+        if (Object.prototype.toString.call(values[i]) !== '[object Array]') {
+            values[i] = [values[i]];
+        }
+        func.apply(this, [values[i][0], values[i][1]]);
+        jasmine.currentEnv_.currentSpec.description += ' (with "' + name + '" using ' + values[i][0].concat(' ') + ')';
+    }
+}
+
+function setValues(title, start, end, startRegime, endRegime) {
+    $('#timelineTitleInput').val(title);
+    $('#timelineStartInput .cz-datepicker-year').val(start);
+    $('#timelineEndInput .cz-datepicker-year').val(end);
+    ($)("#timelineStartInput .cz-datepicker-regime").val(startRegime).attr("selected", "selected");
+    ($)("#timelineEndInput .cz-datepicker-regime").val(endRegime).attr("selected", "selected");
+    $("span:contains('save and close')").click();
+}
 
 function using(name, values, func) {
     for (var i = 0, count = values.length; i < count; i++) {
         if (Object.prototype.toString.call(values[i]) !== '[object Array]') {
             values[i] = [values[i]];
         }
-        func.apply(this, [values[i][0], values[i][1], values[i][2], values[i][3],values[i][4]]);
-        jasmine.currentEnv_.currentSpec.description += ' ' + name + '[' + "title: " + values[i][0] + ", start: " + values[i][1] + ", end: " + values[i][2] + ", regime: " + values[i][3] + ", description: " + values[i][4].concat(']');
+        func.apply(this, [values[i][0], values[i][1], values[i][2], values[i][3], values[i][4], values[i][5]]);
+        jasmine.currentEnv_.currentSpec.description += ' ' + name + '[' + "title: " + values[i][0] + ", start: " + values[i][1] + ", end: " + values[i][2] + ", regimeStart: " + values[i][3] + ", regimeEnd: " + values[i][4] + ", description: " + values[i][5].concat(']');
     }
 }
 
@@ -91,14 +196,14 @@ function init() {
     $('#timelineEndInput').length == 0 ? $('body').prepend('<div id="timelineEndInput"</div>') : "";
     $('#timelineTitleInput').length == 0 ? $('body').prepend('<span id="timelineTitleInput"</span>') : "";
     $('#timelineStartInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ga").text("Ga")).attr("selected", "");
-    $('#timelineStartInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ma").text("Ma"));
-    $('#timelineStartInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ka").text("Ka"));
+    $('#timelineStartInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ma").text("Ma")).attr("selected", "");;
+    $('#timelineStartInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ka").text("Ka")).attr("selected", "");;
     $('#timelineStartInput .cz-datepicker-regime').append($("<option></option>").attr("value", "bce").text("BCE")).attr("selected", "");
-    $('#timelineStartInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ce").text("CE"));
+    $('#timelineStartInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ce").text("CE")).attr("selected", "");;
 
     $('#timelineEndInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ga").text("Ga")).attr("selected", "");
-    $('#timelineEndInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ma").text("Ma"));
-    $('#timelineEndInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ka").text("Ka"));
+    $('#timelineEndInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ma").text("Ma")).attr("selected", "");;
+    $('#timelineEndInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ka").text("Ka")).attr("selected", "");;
     $('#timelineEndInput .cz-datepicker-regime').append($("<option></option>").attr("value", "bce").text("BCE")).attr("selected", "");
-    $('#timelineEndInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ce").text("CE"));
+    $('#timelineEndInput .cz-datepicker-regime').append($("<option></option>").attr("value", "ce").text("CE")).attr("selected", "");;
 }
