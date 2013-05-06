4a029734a7c23460e64541acb2a47b3d267fc769
alexey chumagin <v-alchum@microsoft.com>
2013-05-06 18:32:23 +0400

    added initialize and cleanup

--- Test/chronozoom20.test/Tests.UI/SearchTests.cs
+++ Test/chronozoom20.test/Tests.UI/SearchTests.cs
@@ -5,6 +5,34 @@ namespace Tests
     [TestClass]
     public class SearchTests : TestBase
     {
-         
+        public TestContext TestContext { get; set; }
+
+        #region Initialize and Cleanup
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            BrowserStateManager.RefreshState();
+            NavigationHelper.OpenHomePage();
+            WelcomeScreenHelper.CloseWelcomePopup();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        #endregion
     }
 }
