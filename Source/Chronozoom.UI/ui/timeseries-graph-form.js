1926767ba874935f6e2a3d3cfde07f511c59785a
Nikita A. Skoblov <nikita@fox.mslab.cs.msu.su>
2013-05-13 16:10:47 +0400

    timeSeries form cleanup

--- Source/Chronozoom.UI/ui/timeseries-graph-form.js
+++ Source/Chronozoom.UI/ui/timeseries-graph-form.js
@@ -7,18 +7,6 @@ var TimeSeriesForm = (function (_super) {
     __extends(TimeSeriesForm, _super);
     function TimeSeriesForm(container, formInfo) {
         _super.call(this, container, formInfo);
-        this.testButton = container.find(formInfo.testButton);
-        this.testButton.click(function (event) {
-            alert("It Works!");
-        });
     }
-    TimeSeriesForm.prototype.show = function () {
-        _super.prototype.show.call(this);
-        this.activationSource.addClass("activeButton");
-    };
-    TimeSeriesForm.prototype.close = function () {
-        _super.prototype.close.call(this);
-        this.activationSource.removeClass("activeButton");
-    };
     return TimeSeriesForm;
 })(CZ.UI.FormBase);
