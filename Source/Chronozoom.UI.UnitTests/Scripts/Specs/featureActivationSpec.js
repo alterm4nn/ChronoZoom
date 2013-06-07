710d33c93f5129b6f56110333152dbb11e3ab90b
Eugene W. Nourminsky <eugene@mstlab.org>
2013-06-07 11:52:57 +0400

    [TestFix] Feature activation test fix

--- Source/Chronozoom.UI.UnitTests/Scripts/Specs/featureActivationSpec.js
+++ Source/Chronozoom.UI.UnitTests/Scripts/Specs/featureActivationSpec.js
@@ -1,3 +1,10 @@
+/// <reference path="../typings/jasmine/jasmine.d.ts" />
+/// <reference path="../../../chronozoom.ui/scripts/typings/jquery/jquery.d.ts"/>
+/// <chutzpah_reference path="../../../chronozoom.ui/scripts/external/jquery-1.7.2.min.js" />
+
+/// <reference path="../../../chronozoom.ui/scripts/cz.js" />
+
+
 describe("featureActivation controller", function () {
     it("recognizes enabled feature as enabled", function () {
         var featureMap = [
