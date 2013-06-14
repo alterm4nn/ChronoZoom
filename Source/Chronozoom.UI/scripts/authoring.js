6e0c7dd056a2d039eee4d474970767a0e75d6efe
alex-zen <alexz@mstlab.org>
2013-06-14 15:53:45 +0400

    [Hotfix] Bug #663. Timeline: Incorrect time interval for timelines which "end" is less than 1 BCE.

--- Source/Chronozoom.UI/scripts/authoring.js
+++ Source/Chronozoom.UI/scripts/authoring.js
@@ -301,7 +301,7 @@ var CZ;
             var temp = {
                 x: Number(prop.start),
                 y: t.y,
-                width: Number(CZ.Dates.getCoordinateFromDecimalYear(prop.end) - prop.start),
+                width: prop.end === 9999 ? Number(CZ.Dates.getCoordinateFromDecimalYear(prop.end) - prop.start) : Number(prop.end - prop.start),
                 height: t.height,
                 type: "rectangle"
             };
