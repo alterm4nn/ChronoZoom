dbe56aed18157abe0992c192c0c82aed2daeb0ed
alterm4nn <eugene@mstlab.org>
2014-03-24 17:02:44 +0400

    Fixing build, reconfiguring project, update cz.js

--- Source/Chronozoom.UI/ui/auth-edit-exhibit-form.js
+++ Source/Chronozoom.UI/ui/auth-edit-exhibit-form.js
@@ -315,8 +315,8 @@ var CZ;
             };
 
             FormEditExhibit.prototype.close = function (noAnimation) {
-                if (typeof noAnimation === "undefined") { noAnimation = false; }
                 var _this = this;
+                if (typeof noAnimation === "undefined") { noAnimation = false; }
                 if (this.isModified) {
                     if (window.confirm("There is unsaved data. Do you want to close without saving?")) {
                         this.isModified = false;
