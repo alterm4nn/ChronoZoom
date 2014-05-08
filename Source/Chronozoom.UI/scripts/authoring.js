db5bca1d65e8e4eea057dbbc9130880b89b9f506
Eugene W. Nourminsky <nourminsky@gmail.com>
2014-05-09 02:52:39 +0400

    Fix for onedrive issue - #1257

--- Source/Chronozoom.UI/scripts/authoring.js
+++ Source/Chronozoom.UI/scripts/authoring.js
@@ -617,16 +617,16 @@
                         }
                     }
                 } else if (ci.mediaType.toLowerCase() === "skydrive-document") {
-                    var skydrive = /skydrive\.live\.com\/embed/;
+                    var skydrive = /(onedrive|skydrive)\.live\.com\/embed/;
 
                     if (!skydrive.test(ci.uri)) {
-                        alert("This is not a Skydrive embed link.");
+                        alert("This is not a OneDrive embed link.");
                         isValid = false;
                     }
                 } else if (ci.mediaType.toLowerCase() === "skydrive-image") {
                     var splited = ci.uri.split(' ');
 
-                    var skydrive = /skydrive\.live\.com\/embed/;
+                    var skydrive = /(onedrive|skydrive)\.live\.com\/embed/;
 
                     var width = /[0-9]/;
 
@@ -634,7 +634,7 @@
 
                     if (!skydrive.test(splited[0]) || !width.test(splited[1]) || !height.test(splited[2])) {
                         if (mediaInput) {
-                            mediaInput.showError("This is not a Skydrive embed link.");
+                            mediaInput.showError("This is not a OneDrive embed link.");
                         }
 
                         isValid = false;
