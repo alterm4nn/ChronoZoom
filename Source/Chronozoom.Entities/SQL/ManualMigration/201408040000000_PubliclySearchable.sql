9f83e6c1770e7bd76995b7113b9f5335dc742cf2
Neil Cresswell <neil@cresswell.net>
2014-08-08 18:50:35 -0700

    Search: Make Cosmos Publicly Searchable
    Override to ensure that Cosmos' parent collection always appears in
    public search results when user searches over all collections.

--- Source/Chronozoom.Entities/SQL/ManualMigration/201408040000000_PubliclySearchable.sql
+++ Source/Chronozoom.Entities/SQL/ManualMigration/201408040000000_PubliclySearchable.sql
@@ -3,6 +3,15 @@
 ALTER TABLE [Collections] ADD [PubliclySearchable] BIT NOT NULL DEFAULT (0);
 GO
 
+-- Default is that all existing collections are not publicly searchable, and
+-- new collections when created will be publicly searchable, (publicly searchable set to true in code at creation.)
+-- However, we need a special exception to make Cosmos' collection publicly searchable, if it already exists:
+UPDATE  [Collections]
+SET     PubliclySearchable = 1
+WHERE   Id IN
+(
+    SELECT Collection_Id From [Timelines] WITh (NOLOCK) WHERE Id = '00000000-0000-0000-0000-000000000000'
+);
 
 -- note transformation completed --
 
